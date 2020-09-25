5011f8618ac02b9993c72fec5dbbb124b67aac05
bbarello <bryanbarello@gmail.com>
2020-09-24 19:22:22 -0700

    added prompts and validations

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
-var password = " ";
-var characterLength = " ";
-var characterType = " ";
-var specialCharacter ="@!$%&*";
-i = 0;
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric ="012345789012345789012345789"
+var specialCharacter ="!@#$%^&*(){}[]=<>/,";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
 
-  characterLength = prompt("Please enter password length between 8 and 128 characters!");
-  characterType = prompt("Please choose lowercase, uppercase, numeric, and/or special characters!");
+  var password = "";
+
+
+   //  character length user prompt
+   var characterLength = Number(prompt("Please enter password length between 8 and 128 characters!"));
+   
+   // while loop character length validator alert and retry
+   while(characterLength <= 7 || characterLength >= 129) {
+    alert("Password length must be between 8-128 characters. Please try again!");
+    var characterLength = (prompt("How many characters would you like your password to contain?"));
+    } 
   
-  for (i = 1; i <= 8; i++){
+  // character type user prompt
+  var characterType = prompt("Please choose lowercase, uppercase, numeric, and/or special characters!");
+  
+  // while loop character type validator alert and retry
+  while(characterType !== "uppercase" && characterType !== "lowercase" && characterType !== "numeric" && characterType !== "special characters") {
+    alert("Please choose lowercase, uppercase, numeric, and/or special characters!");
+    var characterType = (prompt("Please choose lowercase, uppercase, numeric, and/or special characters!"));
+    } 
+
+  // for loop to generate password based on selected criteria
+  for (var i = 0; i < characterLength; i++){
     var char = Math.floor(Math.random()
-            * str.length + 1);
-     
+            * characterType.length + 1  );
+  
+  // if statements for character type
+      if(characterType == "lowercase"){
+       password += str.charAt(char).toLowerCase()
+      }
+      if(characterType == "uppercase"){
         password += str.charAt(char)
+      }
+      if(characterType == "numeric"){
+        password += numeric.charAt(char)
+      }
+      if(characterType == "special characters"){
+        password += specialCharacter.charAt(char)
+      }
+  
 
       //  console.log(password);
 }
